fe4882f65547e051c9bd0f9eeeecba2fcc72df0e
d_yanis <dahmane.yanis.2004@gmail.com>
2023-09-09 18:03:46 +0200

    Création de classe Game

--- src/main.py
+++ src/main.py
@@ -1,26 +1,32 @@
 import pygame as py
 
-# pygame setup
-py.init()
-screen = py.display.set_mode((1280, 720))
-clock = py.time.Clock()
-running = True
 
-while running:
-    # poll for events
-    # py.QUIT event means the user clicked X to close your window
-    for event in py.event.get():
-        if event.type == py.QUIT:
-            running = False
+class Game:
+    def __init__(self):
+        return 
+    
+    def play():
+        # pygame setup
+        py.init()
+        screen = py.display.set_mode((1280, 720))
+        clock = py.time.Clock()
+        running = True
 
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
+        while running:
+            # poll for events
+            # py.QUIT event means the user clicked X to close your window
+            for event in py.event.get():
+                if event.type == py.QUIT:
+                    running = False
 
-    # RENDER YOUR GAME HERE
+            # fill the screen with a color to wipe away anything from last frame
+            screen.fill("purple")
 
-    # flip() the display to put your work on screen
-    py.display.flip()
+            # RENDER YOUR GAME HERE
 
-    clock.tick(60)  # limits FPS to 60
+            # flip() the display to put your work on screen
+            py.display.flip()
 
-py.quit()
+            clock.tick(60)  # limits FPS to 60
+
+        py.quit()
